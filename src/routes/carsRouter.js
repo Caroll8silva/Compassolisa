15ef278415c488d756d29dff98a8d3a664bcb624
unknown <carolinne.silva888@gmail.com>
2022-01-27 01:36:49 -0300

    feat:creating post route validation
    validating if the year of the car is greater than 1950 and less than 2022, if it has at least 1 accessory and if the accessories are unique

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -1,7 +1,8 @@
-const carsController = require('../app/controllers/carsController');
+const carsController = require('../app/controllers/carsController')
+const postValidation = require('../app/validation/cars/createCar')
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-  routes.post('/', carsController.createCar)
+  routes.post('/', postValidation, carsController.createCar)
   routes.get('/', carsController.getAllCars)
   routes.get('/:id', carsController.getOneCar)
   routes.delete('/:id', carsController.deleteCar)
