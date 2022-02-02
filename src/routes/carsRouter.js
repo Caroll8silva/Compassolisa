16fbca9d4998ed5042026d7f84563ba377d23a88
unknown <carolinne.silva888@gmail.com>
2022-02-02 19:18:19 -0300

    feat: create car post route validation

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -1,10 +1,11 @@
 const carsController = require('../app/controllers/carsController');
 const postValidation = require('../app/validation/cars/createCar');
+const getValidation = require('../app/validation/cars/getCar');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.post('/', postValidation, carsController.createCar);
-  routes.get('/', carsController.getAllCars);
-  routes.get('/:id', carsController.getOneCar);
+  routes.get('/', getValidation, carsController.getAllCars);
+  routes.get('/:id', getValidation, carsController.getOneCar);
   routes.delete('/:id', carsController.deleteCar);
   routes.put('/:id', postValidation, carsController.updateCar);
   server.use(prefix, routes); 
