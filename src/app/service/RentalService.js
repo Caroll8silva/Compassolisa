1cf80fe56f9233947f28faa6655fd244e54eda4a
unknown <carolinne.silva888@gmail.com>
2022-02-11 21:22:07 -0300

    feat: create rental filial validation

--- src/app/service/RentalService.js
+++ src/app/service/RentalService.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-await-in-loop */
+const NotUnique = require('../errors/NotUnique');
+const BadRequest = require('../errors/BadRequest');
 const RentalRepository = require('../repository/RentalRepository');
 const isUnique = require('../utils/rental/isUniqueCnpj');
 
 class RentalService { 
 
   async create(payload) {
-  
+
+    const address = payload.endereco;
     for (let countCEP = 0; countCEP < payload.endereco.length; countCEP += 1) {
-      const address = payload.endereco;
       const response = address[countCEP];
       const data = await RentalRepository.findCep(response.cep);
       const { cep, logradouro, complemento, bairro, localidade, uf } = data;
@@ -17,6 +19,16 @@ class RentalService {
       response.bairro = bairro;
       response.localidade = localidade;
       response.uf = uf;
+      
+    }
+    const countIsFilial = address.filter((value) => value.isFilial !== true);
+
+    if (countIsFilial.length === 0 )   {
+      throw new BadRequest('There is no parent rental company in addresses ');
+    } 
+    else if( countIsFilial.length > 1 ){
+      throw new NotUnique('There can only be one parent rental company');
+      
     }
 
     await isUnique(payload.cnpj);
