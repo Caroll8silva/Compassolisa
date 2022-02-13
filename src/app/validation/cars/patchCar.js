97aa0666595ad5ff9e6b55f64ca29b41743fe7bb
unknown <carolinne.silva888@gmail.com>
2022-02-13 00:00:29 -0300

    feat: create descricao car validation

---
+++ src/app/validation/cars/patchCar.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+module.exports = async (req, res, next) => {
+  try {
+    const schema = Joi.object({
+    
+      descricao: Joi
+        .string()
+        .min(3)
+        .required()
+
+    });
+       
+  
+    const { error } = await schema.validate(req.body, { abortEarly: false });
+    if (error) throw error;
+    return next();
+  } catch (error) {
+    return res.status(400).json({'description:': error.name, 'name:': error.message});
+  }
+};
