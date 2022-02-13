f41b8aa61336f65c2fc948fb7adcb7e781ec07a3
unknown <carolinne.silva888@gmail.com>
2022-02-13 00:03:52 -0300

    feat: add patch acessory validation in route folder

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -1,6 +1,7 @@
 const carsController = require('../app/controllers/CarsController');
 const postValidation = require('../app/validation/cars/createCar');
 const getValidation = require('../app/validation/cars/getCar');
+const patchValidation = require('../app/validation/cars/patchCar');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.post('/', postValidation, carsController.createCar);
@@ -8,6 +9,6 @@ module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.get('/:id', getValidation, carsController.getOneCar);
   routes.delete('/:id', carsController.deleteCar);
   routes.put('/:id', getValidation, carsController.updateCar);
-  routes.patch('/:id/acessorios/:id2', carsController.updateAcessories);
+  routes.patch('/:id/acessorios/:id2', patchValidation, carsController.updateAcessories);
   server.use(prefix, routes); 
 };
