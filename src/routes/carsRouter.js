aa80e328927161e29c52ead80d33f74ea24a92b4
unknown <carolinne.silva888@gmail.com>
2022-02-15 19:03:35 -0300

    feat: create token authentication in car route

--- src/routes/carsRouter.js
+++ src/routes/carsRouter.js
@@ -2,13 +2,14 @@ const carsController = require('../app/controllers/CarsController');
 const postValidation = require('../app/validation/cars/createCar');
 const getValidation = require('../app/validation/cars/getCar');
 const patchValidation = require('../app/validation/cars/patchCar');
+const authentication = require('../app/validation/authentication');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-  routes.post('/', postValidation, carsController.createCar);
-  routes.get('/', getValidation, carsController.getAllCars);
-  routes.get('/:id', getValidation, carsController.getOneCar);
-  routes.delete('/:id', carsController.deleteCar);
-  routes.put('/:id', getValidation, carsController.updateCar);
-  routes.patch('/:id/acessorios/:id2', patchValidation, carsController.updateAcessories);
+  routes.post('/', authentication, postValidation, carsController.createCar);
+  routes.get('/', authentication, getValidation, carsController.getAllCars);
+  routes.get('/:id', authentication, getValidation, carsController.getOneCar);
+  routes.delete('/:id', authentication, carsController.deleteCar);
+  routes.put('/:id', authentication, getValidation, carsController.updateCar);
+  routes.patch('/:id/acessorios/:id2', authentication,  patchValidation, carsController.updateAcessories);
   server.use(prefix, routes); 
 };
