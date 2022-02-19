be7a6f7dd28104002925674617f373b27139f856
unknown <carolinne.silva888@gmail.com>
2022-02-19 19:56:47 -0300

    refactor: refactor delete car test

--- __tests__/integration/Cars/deleteCars.test.js
+++ __tests__/integration/Cars/deleteCars.test.js
@@ -1,22 +1,65 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const carSchema = require("../../../src/app/schema/cars");
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
+
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
+
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
+  });
+
+  const { body } = response;
+  token = body.token;
+
+});
+
+beforeEach(async () => {
+
+  await carSchema.deleteMany();
+  await peopleSchema.deleteMany();
 });
 
 describe("deleting a car", () => {
+ 
+  const carsInformation = {
+    modelo: 'GM S10 2.5',
+    cor: 'branco',
+    ano: '2021',
+    acessorios: [
+      { descricao: 'Tração 4x4' },
+      { descricao: '4 portas' },
+      { descricao: 'Diesel' },
+      { descricao: 'Air bag' },
+      { descricao: 'ABS' }
+    ],
+    quantidadePassageiros: 5
+  };
+
+  it("Should be possible to delete a car", async () => {
+    const { text } = await supertest(app).post('/api/v1/car/').set('Authorization', `Bearer ${token}`).send(carsInformation);    
+    const { _id } = JSON.parse(text);
+    await supertest(app).get('/api/v1/car/').set('Authorization', `Bearer ${token}`);
+    const response = await supertest(app).delete(`/api/v1/car/${_id.toString()}`).set('Authorization', `Bearer ${token}`);
     
-
-  it("It should be possible to delete a car", async () => {
-
-    const response = await supertest(app).delete('/api/v1/car/:id');
-
-    expect(response.status);
+    const { status } = response;
+    expect(status).toBe(204);
   });
 
 });
