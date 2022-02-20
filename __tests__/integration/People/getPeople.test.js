b1294f948c4aba5c0abcda021b19aaa40ba78ed0
unknown <carolinne.silva888@gmail.com>
2022-02-19 21:25:21 -0300

    refactor: get people test

--- __tests__/integration/People/getPeople.test.js
+++ __tests__/integration/People/getPeople.test.js
@@ -1,22 +1,66 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
+
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
+
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
+  });
+
+  const { body } = response;
+  token = body.token;
+
 });
 
-describe("listing a person", () => {
-    
+beforeEach(async () => {
 
-  it("It should be possible to list a person", async () => {
+  await peopleSchema.deleteMany();
+});
 
-    const response = await supertest(app).get('/api/v1/people');
+describe("listing people", () => {
+  
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
 
-    expect(response.status);
+  it("Should be possible to list people", async () => {
+
+    await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);
+    const response = await supertest(app).get('/api/v1/car/').set('Authorization', `Bearer ${token}`);
+  
+    const { status } = response;
+    expect(status).toBe(200);
+
   });
- 
+  it("Should be possible to give an error when listing sintax", async () => {
+
+    const response = await supertest(app).get('/api/v1/peopl');
+  
+    const { status } = response;
+    expect(status).toBe(404);
+
+  });
 });
