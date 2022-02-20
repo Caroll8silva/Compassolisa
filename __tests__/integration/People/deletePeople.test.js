e44b7815cd9ca258aa7cf44c5dab7173a5f9b460
unknown <carolinne.silva888@gmail.com>
2022-02-19 21:19:34 -0300

    refactor: delete people test

--- __tests__/integration/People/deletePeople.test.js
+++ __tests__/integration/People/deletePeople.test.js
@@ -1,22 +1,72 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
+
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
+
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
+  });
+
+  const { body } = response;
+  token = body.token;
+
+});
+
+beforeEach(async () => {
+
+  await peopleSchema.deleteMany();
 });
 
 describe("deleting a person", () => {
+ 
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+  it("Should be possible to delete a person", async () => {
     
+    const { text } = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);    
+    const { _id } = JSON.parse(text);
+    await supertest(app).get('/api/v1/people/').set('Authorization', `Bearer ${token}`);
+    const response = await supertest(app).delete(`/api/v1/people/${_id.toString()}`).set('Authorization', `Bearer ${token}`);
+    
+    const { status } = response;
+    expect(status).toBe(204);
+ 
+  });
 
-  it("It should be possible to delete a person", async () => {
+  it("Should be possible to throw an error if you can't delete", async () => {
 
-    const response = await supertest(app).get('/api/v1/people/:id');
 
-    expect(response.status);
+    const deleteError = await supertest(app).delete('/api/v1/people/:id');
+
+    const { status } = deleteError;
+    expect(status).toBe(400);
+
+
+    
   });
 
 });
