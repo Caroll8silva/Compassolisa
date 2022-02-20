e38fa7561002c95c681fb97750b812b2e0173e36
unknown <carolinne.silva888@gmail.com>
2022-02-19 21:16:08 -0300

    refactor: refactor create people test

--- __tests__/integration/People/createPeople.test.js
+++ __tests__/integration/People/createPeople.test.js
@@ -1,29 +1,69 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
-});
 
-describe("creating a person", () => {
-    
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
 
-  it("It should be possible to create a person", async () => {
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
 
-    const response = await supertest(app).post('/api/v1/people').send({
-      nome: "joaozinho ciclano",
-      cpf: "131.147.860-49",
-      data_nascimento: "03/03/2021",
-      email: "joazinho@email.com",
-      senha: "123456",
-      habilitado: "sim"
-    });
-
-    expect(response.status);
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
   });
 
+  const { body } = response;
+  token = body.token;
+
+});
+
+beforeEach(async () => {
+
+  await peopleSchema.deleteMany();
+});
+  
+describe("creating a people", () => {
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+    
+  };
+  it("Should be possible to create a people", async () => {
+    
+    const response = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`)
+      .send(peopleInformation);
+      
+    const { status } = response;
+    expect(status).toBe(201);
+  });
+  it("Should return error if user does not submit all required data", async () => {
+
+    const incompleteInformation = {
+      nome: "joaozinho ciclano",
+      data_nascimento: "03/03/2000",
+      email: "joazinho123@email.com"
+    };
+
+    const response = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`)
+      .send(incompleteInformation);
+      
+    const { status } = response;
+    expect(status).toBe(400);
+  });
 });
