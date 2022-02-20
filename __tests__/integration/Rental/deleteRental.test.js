6c617fbb9f24de80ef3a85a6e240f7deab399cca
unknown <carolinne.silva888@gmail.com>
2022-02-19 22:05:26 -0300

    feat: create delete car test

---
+++ __tests__/integration/Rental/deleteRental.test.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-undef */
+const supertest = require('supertest');
+const rentalSchema = require("../../../src/app/schema/rental");
+const peopleSchema = require("../../../src/app/schema/people");
+const app = require('../../infra/AppTest');
+
+
+beforeAll(async () => {
+
+  await rentalSchema.deleteMany();
+  await peopleSchema.deleteMany();
+
+});
+
+beforeEach(async () => {
+
+  await rentalSchema.deleteMany();
+  await peopleSchema.deleteMany();
+});
+
+describe("deleting a rental", () => {
+ 
+  const rentalInformation = {
+    nome: "Localiza Rent a Car",
+    cnpj: "16.670.085/0001-55",
+    atividades: "Aluguel de Carros E Gestão de Frotas",
+    endereco: [ 
+      {
+        cep: "96200-200",
+        number:"1234",
+        isFilial: false
+      }  
+    ]
+  };
+
+  it("Should be possible to delete a rental", async () => {
+    
+    const { text } = await supertest(app).post('/api/v1/rental/').send(rentalInformation);    
+    const { _id } = JSON.parse(text);
+    await supertest(app).get('/api/v1/rental/');
+    const response = await supertest(app).delete(`/api/v1/rental/${_id.toString()}`);
+    
+    const { status } = response;
+    expect(status).toBe(204);
+ 
+  });
+
+  it("Should be possible to throw an error if you can't delete", async () => {
+
+
+    const deleteError = await supertest(app).delete('/api/v1/rental/:id');
+
+    const { status } = deleteError;
+    expect(status).toBe(400);
+
+
+    
+  });
+
+});
