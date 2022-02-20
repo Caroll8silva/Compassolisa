8d6b52dc1beb309bf129c1bcbd88a656f02d04af
unknown <carolinne.silva888@gmail.com>
2022-02-19 21:32:42 -0300

    refactor: update people test

--- __tests__/integration/People/getIdPeople.test.js
+++ __tests__/integration/People/getIdPeople.test.js
@@ -1,22 +1,71 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
+
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
+
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
+  });
+
+  const { body } = response;
+  token = body.token;
+
 });
 
-describe("listing only one person", () => {
+beforeEach(async () => {
+
+  await peopleSchema.deleteMany();
+});
+
+
+describe("listing a person", () => {
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  it("Should be possible to list a person", async () => {
     
-
-  it("It should be possible to list only one person", async () => {
-
-    const response = await supertest(app).get('/api/v1/people/:id');
-
-    expect(response.status);
+    const { text } = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);
+    const { _id } = JSON.parse(text);
+    const response = await supertest(app).get(`/api/v1/people/${_id.toString()}`).set('Authorization', `Bearer ${token}`);
+    
+    const { status } = response;
+    expect(status).toBe(200);
   });
+
+  it("Should be possible to give an error when listing sintax", async () => { 
+    
+    const { text } = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);
+    const { _id } = JSON.parse(text);
+    const response = await supertest(app).get(`/api/v1/peopl/${_id.toString()}`);
+
+    const { status } = response;
+    expect(status).toBe(404);
+
+  });
+
 
 });
--- __tests__/integration/People/putPeople.test.js
+++ __tests__/integration/People/putPeople.test.js
@@ -1,29 +1,81 @@
 /* eslint-disable no-undef */
 const supertest = require('supertest');
-const { resolve } = require('path');
-const dotEnv = require('dotenv');
+const peopleSchema = require("../../../src/app/schema/people");
 const app = require('../../infra/AppTest');
 
-dotEnv.config({
+let token;
+beforeAll(async () => {
 
-  path: resolve(__dirname, '..', '.env.test')
+
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  await supertest(app).post('/api/v1/people/').send(peopleInformation);
+
+  const response = await supertest(app).post('/api/v1/authenticate/').send({ 
+    email: peopleInformation.email, 
+    senha: peopleInformation.senha 
+  });
+
+  const { body } = response;
+  token = body.token;
+
 });
 
-describe("updating a person", () => {
-    
+beforeEach(async () => {
 
-  it("It should be possible to update a person", async () => {
+  await peopleSchema.deleteMany();
+});
 
-    const response = await supertest(app).put('/api/v1/people/:id').send({
-      nome: "joaozinho ciclano",
-      cpf: "131.147.860-49",
-      data_nascimento: "03/03/2021",
-      email: "joazinho@email.com",
-      senha: "123456",
-      habilitado: "sim"
-    });
+describe("updating people", () => {
+  const peopleInformation = {
+    nome: "joaozinho ciclano",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "joazinho@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
 
-    expect(response.status);
+  const updateInformation = {
+    nome: "maria ciclana",
+    cpf: "131.147.860-49",
+    data_nascimento: "03/03/2000",
+    email: "maria@email.com",
+    senha: "123456",
+    habilitado: "sim"
+  
+  };
+
+  it("Should be possible to updating people", async () => {
+
+    const { text } = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);
+    const { _id } = JSON.parse(text);
+    await supertest(app).get('/api/v1/people/').set('Authorization', `Bearer ${token}`);
+    const response = await supertest(app).put(`/api/v1/people/${_id.toString()}`).set('Authorization', `Bearer ${token}`).send(updateInformation);;
+
+    const { status } = response;
+    expect(status).toBe(200);
+  });
+
+  it("Should be possible to give error when updating", async () => {
+
+    const { text } = await supertest(app).post('/api/v1/people/').set('Authorization', `Bearer ${token}`).send(peopleInformation);
+    const { _id } = JSON.parse(text);
+    await supertest(app).get('/api/v1/people/').set('Authorization', `Bearer ${token}`);
+    const response = await supertest(app).put(`/api/v1/peopl/${_id.toString()}`).send(updateInformation);;
+
+    const { status } = response;
+    expect(status).toBe(404);
+
   });
 
 });
